2b7c8370c0d556a35f854a4efbb35c7af696f67e
Vitor Taichi Taira <vitorttaira@gmail.com>
2026-06-23 00:59:25 -0300

    refactor: home view model

--- Mobile/app/src/main/java/br/ufscar/cinemiranha/MainActivity.kt
+++ Mobile/app/src/main/java/br/ufscar/cinemiranha/MainActivity.kt
@@ -3,25 +3,10 @@ package br.ufscar.cinemiranha
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.navigation.NavType
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import br.ufscar.cinemiranha.ui.screens.HomeScreen
-import br.ufscar.cinemiranha.ui.screens.MovieDetailScreen
-import br.ufscar.cinemiranha.ui.screens.SessionsScreen
-import br.ufscar.cinemiranha.ui.screens.SeatsScreen
-import br.ufscar.cinemiranha.ui.screens.TicketsScreen
-import br.ufscar.cinemiranha.ui.screens.SnacksScreen
-import br.ufscar.cinemiranha.ui.screens.OrderSummaryScreen
-import br.ufscar.cinemiranha.ui.screens.PaymentMethodScreen
-import br.ufscar.cinemiranha.ui.screens.CardDetailsScreen
-import br.ufscar.cinemiranha.ui.screens.SuccessScreen
+import br.ufscar.cinemiranha.navigation.MainAppNavigation
 import br.ufscar.cinemiranha.ui.theme.CineMiranhaTheme
-import br.ufscar.cinemiranha.viewmodel.CheckoutViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,149 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContent {
             CineMiranhaTheme {
                 val navController = rememberNavController()
-
-                val checkoutViewModel: CheckoutViewModel = viewModel()
-
-                NavHost(navController = navController, startDestination = "home") {
-                    composable("home") {
-                        HomeScreen(
-                            onMovieClick = { movieId ->
-                                checkoutViewModel.resetCheckout()
-                                navController.navigate("movie/$movieId")
-                            }
-                        )
-                    }
-                    composable(
-                        route = "movie/{movieId}",
-                        arguments = listOf(navArgument("movieId") { type = NavType.LongType })
-                    ) { backStackEntry ->
-                        val movieId = backStackEntry.arguments!!.getLong("movieId")
-                        MovieDetailScreen(
-                            movieId = movieId,
-                            onBack = { navController.popBackStack() },
-                            onBuyTickets = { navController.navigate("sessions/$movieId") }
-                        )
-                    }
-                    composable(
-                        route = "sessions/{movieId}",
-                        arguments = listOf(navArgument("movieId") { type = NavType.LongType })
-                    ) { backStackEntry ->
-                        val movieId = backStackEntry.arguments!!.getLong("movieId")
-                        SessionsScreen(
-                            movieId = movieId,
-                            onBack = { navController.popBackStack() },
-                            onSessionSelected = { sessionId ->
-                                navController.navigate("seats/$movieId/$sessionId")
-                            }
-                        )
-                    }
-                    composable(
-                        route = "seats/{movieId}/{sessionId}",
-                        arguments = listOf(
-                            navArgument("movieId") { type = NavType.LongType },
-                            navArgument("sessionId") { type = NavType.LongType }
-                        )
-                    ) { backStackEntry ->
-                        val movieId = backStackEntry.arguments!!.getLong("movieId")
-                        val sessionId = backStackEntry.arguments!!.getLong("sessionId")
-                        SeatsScreen(
-                            movieId = movieId,
-                            sessionId = sessionId,
-                            onBack = { navController.popBackStack() },
-                            onSeatsSelected = { seats ->
-                                checkoutViewModel.setSelectedSeats(seats)
-                                navController.navigate("tickets/$movieId/$sessionId")
-                            }
-                        )
-                    }
-                    composable(
-                        route = "tickets/{movieId}/{sessionId}",
-                        arguments = listOf(
-                            navArgument("movieId") { type = NavType.LongType },
-                            navArgument("sessionId") { type = NavType.LongType }
-                        )
-                    ) { backStackEntry ->
-                        val movieId = backStackEntry.arguments!!.getLong("movieId")
-                        val sessionId = backStackEntry.arguments!!.getLong("sessionId")
-                        TicketsScreen(
-                            movieId = movieId,
-                            sessionId = sessionId,
-                            checkoutViewModel = checkoutViewModel,
-                            onBack = { navController.popBackStack() },
-                            onNext = {
-                                navController.navigate("snacks/$movieId/$sessionId")
-                            }
-                        )
-                    }
-                    composable(
-                        route = "snacks/{movieId}/{sessionId}",
-                        arguments = listOf(
-                            navArgument("movieId") { type = NavType.LongType },
-                            navArgument("sessionId") { type = NavType.LongType }
-                        )
-                    ) { backStackEntry ->
-                        val movieId = backStackEntry.arguments!!.getLong("movieId")
-                        val sessionId = backStackEntry.arguments!!.getLong("sessionId")
-                        SnacksScreen(
-                            viewModel = checkoutViewModel,
-                            onBack = { navController.popBackStack() },
-                            onNext = {
-                                navController.navigate("summary/$movieId/$sessionId")
-                            }
-                        )
-                    }
-                    composable(
-                        route = "summary/{movieId}/{sessionId}",
-                        arguments = listOf(
-                            navArgument("movieId") { type = NavType.LongType },
-                            navArgument("sessionId") { type = NavType.LongType }
-                        )
-                    ) { backStackEntry ->
-                        val movieId = backStackEntry.arguments!!.getLong("movieId")
-                        val sessionId = backStackEntry.arguments!!.getLong("sessionId")
-                        OrderSummaryScreen(
-                            movieId = movieId,
-                            sessionId = sessionId,
-                            checkoutViewModel = checkoutViewModel,
-                            onBack = { navController.popBackStack() },
-                            onNext = {
-                                navController.navigate("payment")
-                            }
-                        )
-                    }
-                    composable(route = "payment") {
-                        PaymentMethodScreen(
-                            checkoutViewModel = checkoutViewModel,
-                            onBack = { navController.popBackStack() },
-                            onSelectMethod = { method ->
-                                if (method == "PIX") {
-                                    navController.navigate("success")
-                                } else {
-                                    navController.navigate("card_details")
-                                }
-                            }
-                        )
-                    }
-                    composable(route = "card_details") {
-                        CardDetailsScreen(
-                            ticketCount = checkoutViewModel.getTotalTicketCount(),
-                            ticketTotal = checkoutViewModel.getTotalTicketPrice(),
-                            snackTotal = checkoutViewModel.getTotalSnackPrice(),
-                            onBack = { navController.popBackStack() },
-                            onConfirm = { navController.navigate("success") }
-                        )
-                    }
-                    composable("success") {
-                        SuccessScreen(
-                            onBackToMenu = {
-                                checkoutViewModel.resetCheckout()
-                                navController.navigate("home") {
-                                    popUpTo("home") { inclusive = true }
-                                }
-                            }
-                        )
-                    }
-                }
+                MainAppNavigation(navController = navController)
             }
         }
     }
--- Mobile/app/src/main/java/br/ufscar/cinemiranha/ui/screens/HomeScreen.kt
+++ Mobile/app/src/main/java/br/ufscar/cinemiranha/ui/screens/HomeScreen.kt
@@ -6,28 +6,26 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
+import br.ufscar.cinemiranha.model.MovieResponse
 import br.ufscar.cinemiranha.ui.composable.Home.MovieContent
 import br.ufscar.cinemiranha.ui.composable._shared.BottomBar
 import br.ufscar.cinemiranha.ui.composable._shared.ErrorState
 import br.ufscar.cinemiranha.ui.composable._shared.LoadingState
 import br.ufscar.cinemiranha.ui.composable._shared.TopBar
-import br.ufscar.cinemiranha.viewmodel.HomeViewModel
-
 
 @Composable
 fun HomeScreen(
-    onMovieClick: (Long) -> Unit = {},
-    viewModel: HomeViewModel = viewModel(factory = HomeViewModel.Factory)
+    isLoading: Boolean,
+    errorMessage: String?,
+    nowPlaying: List<MovieResponse>,
+    comingSoon: List<MovieResponse>,
+    onMovieClick: (Long) -> Unit,
+    onRetry: () -> Unit
 ) {
-    val state by viewModel.uiState.collectAsState()
-
     Scaffold(
-        topBar    = { TopBar() },
-        bottomBar = { BottomBar() },
+        topBar         = { TopBar() },
+        bottomBar      = { BottomBar() },
         containerColor = MaterialTheme.colorScheme.background
     ) { padding ->
         Box(
@@ -36,15 +34,15 @@ fun HomeScreen(
                 .padding(padding)
         ) {
             when {
-                state.isLoading -> LoadingState()
-                state.errorMessage != null -> ErrorState(
-                    message = state.errorMessage!!,
-                    onRetry = { viewModel.loadMovies() }
+                isLoading            -> LoadingState()
+                errorMessage != null -> ErrorState(
+                    message = errorMessage,
+                    onRetry = onRetry
                 )
-                else -> MovieContent(
-                    nowPlaying    = state.nowPlayingMovies,
-                    comingSoon    = state.comingSoonMovies,
-                    onMovieClick  = onMovieClick
+                else                 -> MovieContent(
+                    nowPlaying   = nowPlaying,
+                    comingSoon   = comingSoon,
+                    onMovieClick = onMovieClick
                 )
             }
         }
